fb33585688fcbc9177a80c7973890cbcfcd50292
kubilayyazi78 <kubilayyazi@hotmail.com>
2022-09-11 17:02:17 +0300

    Model entity eklendi.

--- src/rentACar/Domain/Entities/Brand.cs
+++ src/rentACar/Domain/Entities/Brand.cs
@@ -6,11 +6,13 @@ public class Brand : Entity
 {
     public string Name { get; set; }
 
+    public virtual ICollection<Model> Models { get; set; }
+
     public Brand()
     {
 
     }
-    public Brand(int id, string name):this()
+    public Brand(int id, string name) : this()
     {
         Id = id;
         Name = name;
