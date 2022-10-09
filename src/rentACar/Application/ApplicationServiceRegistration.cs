1ff688a69568192c31dcc32ce56b62d9608ff4bf
kubilayyazi78 <kubilayyazi@hotmail.com>
2022-10-09 23:24:58 +0300

    JWT Altyapısı oluşturuldu.

--- src/rentACar/Application/ApplicationServiceRegistration.cs
+++ src/rentACar/Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,6 @@
-﻿using Application.Features.Brands.Rules;
+﻿using Application.Features.Auths.Rules;
+using Application.Features.Brands.Rules;
+using Application.Services.AuthService;
 using Core.Application.Pipelines.Validation;
 using FluentValidation;
 using MediatR;
@@ -15,8 +17,10 @@ public static class ApplicationServiceRegistration
         services.AddMediatR(Assembly.GetExecutingAssembly());
 
         services.AddScoped<BrandBusinessRules>();
+        services.AddScoped<AuthBusinessRules>();
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+        services.AddScoped<IAuthService, AuthManager>();
         return services;
     }
 }
--- src/rentACar/Persistence/PersistenceServiceRegistration.cs
+++ src/rentACar/Persistence/PersistenceServiceRegistration.cs
@@ -15,6 +15,10 @@ public static class PersistenceServiceRegistration
 
         services.AddScoped<IBrandRepository, BrandRepository>();
         services.AddScoped<IModelRepository, ModelRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+        services.AddScoped<IOperationClaimRepository, OperationClaimRepository>();
+        services.AddScoped<IUserOperationClaimRepository, UserOperationClaimRepository>();
 
         return services;
     }
